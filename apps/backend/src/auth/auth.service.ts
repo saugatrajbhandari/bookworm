633468694844447b92de1153af2a58d23883db38
saugatrajbhandari <saugatrajbhandari47@gmail.com>
2025-06-23 22:54:12 +0545

    feat(auth): add user registration with email check and password hashing

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import * as jwt from '';
 
 @Injectable()
 export class AuthService {
   constructor(readonly prismaService: PrismaService) {}
 
   async register(data: Prisma.UserCreateInput) {
+    const isUserExist = await this.findUserWithEmail(data.email);
+
+    if (isUserExist) {
+      throw new ConflictException('User already exist');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
     return this.prismaService.user.create({
-      data,
+      data: { ...data, password: hashedPassword },
     });
+  }
+
+  async findUserWithEmail(email: string) {
+    return this.prismaService.user.findUnique({ where: { email } });
   }
 }
